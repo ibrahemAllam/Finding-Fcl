2fcae5a191f62f98e1ce15c842521dd6f7ab9e78
Ahmed5attab <a7med5attab97@gmail.com>
2018-04-28 19:40:29 +0200

    Add files via upload

---
+++ cameramove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    private Transform lookAt;
+    private Vector3 startOffset;
+    // Use this for initialization
+    void Start()    
+    {
+        lookAt = GameObject.FindGameObjectWithTag("Player").transform;
+        startOffset = transform.position - lookAt.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+            //normal camera movement
+            transform.position = lookAt.position + startOffset;
+    }
+}
